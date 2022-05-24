61b4140a16a69012af3cef2eec6ecd628cc047ab
Rylox <in.telo3200@gmail.com>
2022-05-24 19:55:24 +0200

    1476; 1689

---
+++ algorithms/1689.partitioning-into-minimum-number-of-deci-binary-numbers.py
@@ -0,0 +1,71 @@
+#
+# @lc app=leetcode id=1689 lang=python3
+#
+# [1689] Partitioning Into Minimum Number Of Deci-Binary Numbers
+#
+# https://leetcode.com/problems/partitioning-into-minimum-number-of-deci-binary-numbers/description/
+#
+# algorithms
+# Medium (87.78%)
+# Likes:    832
+# Dislikes: 582
+# Total Accepted:    79.2K
+# Total Submissions: 90.2K
+# Testcase Example:  '"32"'
+#
+# A decimal number is called deci-binary if each of its digits is either 0 or 1
+# without any leading zeros. For example, 101 and 1100 are deci-binary, while
+# 112 and 3001 are not.
+#
+# Given a string n that represents a positive decimal integer, return the
+# minimum number of positive deci-binary numbers needed so that they sum up to
+# n.
+#
+#
+# Example 1:
+#
+#
+# Input: n = "32"
+# Output: 3
+# Explanation: 10 + 11 + 11 = 32
+#
+#
+# Example 2:
+#
+#
+# Input: n = "82734"
+# Output: 8
+#
+#
+# Example 3:
+#
+#
+# Input: n = "27346209830709182346"
+# Output: 9
+#
+#
+#
+# Constraints:
+#
+#
+# 1 <= n.length <= 10^5
+# n consists of only digits.
+# n does not contain any leading zeros and represents a positive integer.
+#
+#
+#
+from types import MethodType
+from algo_input import run
+
+
+# @lc code=start
+class Solution:
+
+    def minPartitions(self, n: str) -> int:
+        return int(max(n))
+
+
+# @lc code=end
+if __name__ == "__main__":
+    run(MethodType(Solution.minPartitions, Solution()),
+        [[["20378901289"], 9], [["0"], 0], [["1000101"], 1], [["1000101"], 1]])
