1d184b0c61dfcd4bf6af477dcb6a6a9a1f67db5d
Rylox <in.telo3200@gmail.com>
2022-07-06 08:02:54 +0200

    509

--- algorithms/509.fibonacci-number.py
+++ algorithms/509.fibonacci-number.py
@@ -77,7 +77,7 @@ class Solution:
         return b
 
     # O(1) time complexity
-    # O(N) space complexity
+    # O(1) space complexity (kind of)
     # LOOKUP = [
     #     0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987,
     #     1597,
