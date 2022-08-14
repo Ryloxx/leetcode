41ca460828f421511593dbbbb309408fbdc9d8e5
Rylox <in.telo3200@gmail.com>
2022-08-14 02:52:52 +0200

    30; 235

---
+++ algorithms/235.lowest-common-ancestor-of-a-binary-search-tree.py
@@ -0,0 +1,102 @@
+#
+# @lc app=leetcode id=235 lang=python3
+#
+# [235] Lowest Common Ancestor of a Binary Search Tree
+#
+# https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/
+#
+# algorithms
+# Easy (57.19%)
+# Likes:    7400
+# Dislikes: 221
+# Total Accepted:    928.7K
+# Total Submissions: 1.6M
+# Testcase Example:  '[6,2,8,0,4,7,9,null,null,3,5]\n2\n8'
+#
+# Given a binary search tree (BST), find the lowest common ancestor (LCA) node
+# of two given nodes in the BST.
+#
+# According to the definition of LCA on Wikipedia: “The lowest common ancestor
+# is defined between two nodes p and q as the lowest node in T that has both p
+# and q as descendants (where we allow a node to be a descendant of
+# itself).”
+#
+#
+# Example 1:
+#
+#
+# Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+# Output: 6
+# Explanation: The LCA of nodes 2 and 8 is 6.
+#
+#
+# Example 2:
+#
+#
+# Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+# Output: 2
+# Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant
+# of itself according to the LCA definition.
+#
+#
+# Example 3:
+#
+#
+# Input: root = [2,1], p = 2, q = 1
+# Output: 2
+#
+#
+#
+# Constraints:
+#
+#
+# The number of nodes in the tree is in the range [2, 10^5].
+# -10^9 <= Node.val <= 10^9
+# All Node.val are unique.
+# p != q
+# p and q will exist in the BST.
+#
+#
+#
+from algo_input import run, TreeNode
+from types import MethodType
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode',
+                             q: 'TreeNode') -> 'TreeNode':
+        if root is None:
+            return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        if (left and right) or root.val == p.val or root.val == q.val:
+            return root
+        return left or right
+
+
+# @lc code=end
+if __name__ == "__main__":
+    run(MethodType(Solution.lowestCommonAncestor, Solution()), [
+        [[
+            TreeNode.createTree([6, 2, 8, 0, 4, 7, 9, None, None, 3, 5]),
+            TreeNode(2),
+            TreeNode(8)
+        ], 6],
+        [[
+            TreeNode.createTree([6, 2, 8, 0, 4, 7, 9, None, None, 3, 5]),
+            TreeNode(2),
+            TreeNode(4)
+        ], 2],
+        [[TreeNode.createTree([2, 1]),
+          TreeNode(2), TreeNode(1)], 2],
+    ],
+        comparator=lambda a, b:
+        (a is None and b is None) or (a is not None and a.val == b))
---
+++ algorithms/30.substring-with-concatenation-of-all-words.py
@@ -0,0 +1,216 @@
+#
+# @lc app=leetcode id=30 lang=python3
+#
+# [30] Substring with Concatenation of All Words
+#
+# https://leetcode.com/problems/substring-with-concatenation-of-all-words/description/
+#
+# algorithms
+# Hard (28.19%)
+# Likes:    2880
+# Dislikes: 2122
+# Total Accepted:    295.1K
+# Total Submissions: 984.8K
+# Testcase Example:  '"barfoothefoobarman"\n["foo","bar"]'
+#
+# You are given a string s and an array of strings words of the same length.
+# Return all starting indices of substring(s) in s that is a concatenation of
+# each word in words exactly once, in any order, and without any intervening
+# characters.
+#
+# You can return the answer in any order.
+#
+#
+# Example 1:
+#
+#
+# Input: s = "barfoothefoobarman", words = ["foo","bar"]
+# Output: [0,9]
+# Explanation: Substrings starting at index 0 and 9 are "barfoo" and "foobar"
+# respectively.
+# The output order does not matter, returning [9,0] is fine too.
+#
+#
+# Example 2:
+#
+#
+# Input: s = "wordgoodgoodgoodbestword", words = ["word","good","best","word"]
+# Output: []
+#
+#
+# Example 3:
+#
+#
+# Input: s = "barfoofoobarthefoobarman", words = ["bar","foo","the"]
+# Output: [6,9,12]
+#
+#
+#
+# Constraints:
+#
+#
+# 1 <= s.length <= 10^4
+# 1 <= words.length <= 5000
+# 1 <= words[i].length <= 30
+# s and words[i] consist of lowercase English letters.
+#
+#
+#
+from collections import Counter
+from functools import reduce
+from typing import List
+from algo_input import run
+from types import MethodType
+
+
+# @lc code=start
+class Solution:
+
+    # N = len(s), M = len(words), P = len(words[i])
+
+    # Using a rolling hash
+    # O(M * P + N) time complexity
+    # O(M + N) space complexity
+    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+        if not words:
+            return []
+        words_len, w_len, res, p = len(words), len(words[0]), [], 31
+
+        # This mod is good enough for this Leetcode problem but a higher one
+        # should be safer like 7 + 10**9
+        mod = 389
+
+        words = Counter(
+            reduce(lambda acc, curr: (acc * p + ord(curr)) % mod, w, 0)
+            for w in words)
+        rolling_hash, p_limit = 0, pow(p, w_len, mod)
+        hashes = [
+            rolling_hash :=
+            (rolling_hash * p + ord(s[idx]) -
+             (ord(s[idx - w_len]) * p_limit if idx >= w_len else 0)) % mod
+            for idx in range(len(s))
+        ]
+
+        for i in range(w_len):
+            word_count, start, w_count = Counter(), i + w_len - 1, 0
+            for idx in range(start, len(s), w_len):
+                w_hash = hashes[idx]
+                if w_hash not in words:
+                    word_count, start, w_count = Counter(), idx + w_len, 0
+                    continue
+                while word_count[w_hash] == words[w_hash]:
+                    word_count[hashes[start]] -= 1
+                    start += w_len
+                    w_count -= 1
+                word_count[w_hash] += 1
+                w_count += 1
+                if w_count == words_len:
+                    res.append(idx - w_count * w_len + 1)
+                    word_count[hashes[start]] -= 1
+                    start += w_len
+                    w_count -= 1
+
+        return res
+
+    # Using string slicing
+    # O(M + P * N) time complexity
+    # O(M * P) space complexity
+    # def findSubstring(self, s: str, words: List[str]) -> List[int]:
+    #     if not words:
+    #         return []
+    #     res = []
+    #     len_s, len_words, len_w = len(s), len(words), len(words[0])
+    #     wds = Counter(words)
+    #     for i in range(len_w):
+    #         window, start, c = Counter(), i, 0
+    #         while i < len_s:
+    #             w = s[i:i + len_w]
+    #             if w not in wds:
+    #                 window, start, c = Counter(), i + len_w, 0
+    #             else:
+    #                 window[w] += 1
+    #                 c += 1
+    #                 while window[w] > wds[w]:
+    #                     window[s[start:start + len_w]] -= 1
+    #                     c -= 1
+    #                     start += len_w
+    #                 if c == len_words:
+    #                     res.append(start)
+    #             i += len_w
+    #     return res
+
+
+# @lc code=end
+if __name__ == "__main__":
+    run(
+        MethodType(Solution.findSubstring, Solution()),
+        [
+            [["barfoothefoobarman", ["foo", "bar"]], [0, 9]],
+            [["barfoofoobarthefoobarman", ["bar", "foo", "the"]], [6, 9, 12]],
+            [["ababababab", ["ababa", "babab"]], [0]],
+            [["wordgoodgoodgoodbestword", ["word", "good", "best", "good"]],
+             [8]],
+            [[
+                "pjzkrkevzztxductzzxmxsvwjkxpvukmf"
+                "jywwetvfnujhweiybwvvsrfequzkhossm"
+                "ootkmyxgjgfordrpapjuunmqnxxdrqrfg"
+                "krsjqbszgiqlcfnrpjlcwdrvbumtotzyl"
+                "shdvccdmsqoadfrpsvnwpizlwszrtyclh"
+                "gilklydbmfhuywotjmktnwrfvizvnmfvv"
+                "qfiokkdprznnnjycttprkxpuykhmpchik"
+                "syucbmtabiqkisgbhxngmhezrrqvayfsx"
+                "auampdpxtafniiwfvdufhtwajrbkxtjzq"
+                "jnfocdhekumttuqwovfjrgulhekcpjszy"
+                "ynadxhnttgmnxkduqmmyhzfnjhducesct"
+                "ufqbumxbamalqudeibljgbspeotkgvddc"
+                "wgxidaiqcvgwykhbysjzlzfbupkqunuqt"
+                "raxrlptivshhbihtsigtpipguhbhctcvu"
+                "bnhqipncyxfjebdnjyetnlnvmuxhzsdah"
+                "krscewabejifmxombiamxvauuitoltyym"
+                "sarqcuuoezcbqpdaprxmsrickwpgwpsop"
+                "lhugbikbkotzrtqkscekkgwjycfnvwfgd"
+                "zogjzjvpcvixnsqsxacfwndzvrwrycwxr"
+                "cismdhqapoojegggkocyrdtkzmiekhxop"
+                "pctytvphjynrhtcvxcobxbcjjivtfjiwm"
+                "duhzjokkbctweqtigwfhzorjlkpuuliai"
+                "pbtfldinyetoybvugevwvhhhweejogrgh"
+                "llsouipabfafcxnhukcbtmxzshoyyufjh"
+                "zadhrelweszbfgwpkzlwxkogyogutscvu"
+                "hcllphshivnoteztpxsaoaacgxyaztuix"
+                "hunrowzljqfqrahosheukhahhbiaxqzfm"
+                "mwcjxountkevsvpbzjnilwpoermxrtlfr"
+                "oqoclexxisrdhvfsindffslyekrzwzqkp"
+                "eocilatftymodgztjgybtyheqgcpwogdc"
+                "jlnlesefgvimwbxcbzvaibspdjnrpqtye"
+                "ilkcspknyylbwndvkffmzuriilxagyerj"
+                "ptbgeqgebiaqnvdubrtxibhvakcyotkfo"
+                "nmseszhczapxdlauexehhaireihxsplgd"
+                "gmxfvaevrbadbwjbdrkfbbjjkgcztkcbw"
+                "agtcnrtqryuqixtzhaakjlurnumzyovaw"
+                "rcjiwabuwretmdamfkxrgqgcdgbrdbnug"
+                "zecbgyxxdqmisaqcyjkqrntxqmdrczxbe"
+                "bemcblftxplafnyoxqimkhcykwamvdsxj"
+                "ezkpgdpvopddptdfbprjustquhlazkjfl"
+                "uxrzopqdstulybnqvyknrchbphcarknnh"
+                "hovweaqawdyxsqsqahkepluypwrzjegqt"
+                "doxfgzdkydeoxvrfhxusrujnmjzqrrlxg"
+                "lcmkiykldbiasnhrjbjekystzilrwkzho"
+                "ntwmehrfsrzfaqrbbxncphbzuuxeteshy"
+                "rveamjsfiaharkcqxefghgceeixkdgkub"
+                "oupxnwhnfigpkwnqdvzlydpidcljmflbc"
+                "carbiegsmweklwngvygbqpescpeichmfi"
+                "dgsjmkvkofvkuehsmkkbocgejoiqcnafv"
+                "uokelwuqsgkyoekaroptuvekfvmtxtqsh"
+                "cwsztkrzwrpabqrrhnlerxjojemcxel",
+                [
+                    "dhvf", "sind", "ffsl", "yekr", "zwzq", "kpeo", "cila",
+                    "tfty", "modg", "ztjg", "ybty", "heqg", "cpwo", "gdcj",
+                    "lnle", "sefg", "vimw", "bxcb"
+                ]
+            ], [935]],
+            [["aaa", ["a", "b"]], []],
+            [["", []], []],
+            [["aaa", ["aa", "aa"]], []],
+            [["mississippi", ["mississippis"]], []],
+        ],
+    )
