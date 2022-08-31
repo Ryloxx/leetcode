4b42e4a7ed8e944c67ae9ef5ab6867a97e850731
Rylox <in.telo3200@gmail.com>
2022-08-31 10:01:00 +0200

    417

---
+++ algorithms/417.pacific-atlantic-water-flow.py
@@ -0,0 +1,122 @@
+#
+# @lc app=leetcode id=417 lang=python3
+#
+# [417] Pacific Atlantic Water Flow
+#
+# https://leetcode.com/problems/pacific-atlantic-water-flow/description/
+#
+# algorithms
+# Medium (49.42%)
+# Likes:    4893
+# Dislikes: 976
+# Total Accepted:    261.5K
+# Total Submissions: 501.3K
+# Testcase Example:
+# '[[1,2,2,3,5],[3,2,3,4,4],[2,4,5,3,1],[6,7,1,4,5],[5,1,1,2,4]]'
+#
+# There is an m x n rectangular island that borders both the Pacific Ocean and
+# Atlantic Ocean. The Pacific Ocean touches the island's left and top edges,
+# and the Atlantic Ocean touches the island's right and bottom edges.
+#
+# The island is partitioned into a grid of square cells. You are given an m x n
+# integer matrix heights where heights[r][c] represents the height above sea
+# level of the cell at coordinate (r, c).
+#
+# The island receives a lot of rain, and the rain water can flow to neighboring
+# cells directly north, south, east, and west if the neighboring cell's height
+# is less than or equal to the current cell's height. Water can flow from any
+# cell adjacent to an ocean into the ocean.
+#
+# Return a 2D list of grid coordinates result where result[i] = [ri, ci]
+# denotes that rain water can flow from cell (ri, ci) to both the Pacific and
+# Atlantic oceans.
+#
+#
+# Example 1:
+#
+#
+# Input: heights =
+# [[1,2,2,3,5],[3,2,3,4,4],[2,4,5,3,1],[6,7,1,4,5],[5,1,1,2,4]]
+# Output: [[0,4],[1,3],[1,4],[2,2],[3,0],[3,1],[4,0]]
+# Explanation: The following cells can flow to the Pacific and Atlantic oceans,
+# as shown below:
+# [0,4]: [0,4] -> Pacific Ocean
+# [0,4] -> Atlantic Ocean
+# [1,3]: [1,3] -> [0,3] -> Pacific Ocean
+# [1,3] -> [1,4] -> Atlantic Ocean
+# [1,4]: [1,4] -> [1,3] -> [0,3] -> Pacific Ocean
+# [1,4] -> Atlantic Ocean
+# [2,2]: [2,2] -> [1,2] -> [0,2] -> Pacific Ocean
+# [2,2] -> [2,3] -> [2,4] -> Atlantic Ocean
+# [3,0]: [3,0] -> Pacific Ocean
+# [3,0] -> [4,0] -> Atlantic Ocean
+# [3,1]: [3,1] -> [3,0] -> Pacific Ocean
+# [3,1] -> [4,1] -> Atlantic Ocean
+# [4,0]: [4,0] -> Pacific Ocean
+# ⁠      [4,0] -> Atlantic Ocean
+# Note that there are other possible paths for these cells to flow to the
+# Pacific and Atlantic oceans.
+#
+#
+# Example 2:
+#
+#
+# Input: heights = [[1]]
+# Output: [[0,0]]
+# Explanation: The water can flow from the only cell to the Pacific and
+# Atlantic oceans.
+#
+#
+#
+# Constraints:
+#
+#
+# m == heights.length
+# n == heights[r].length
+# 1 <= m, n <= 200
+# 0 <= heights[r][c] <= 10^5
+#
+#
+#
+from itertools import product
+from typing import List, Tuple
+from algo_input import run, any_order
+from types import MethodType
+
+
+# @lc code=start
+class Solution:
+
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        m, n = len(heights), len(heights[0])
+        res = [[0] * n for _ in range(m)]
+
+        def fill(pos: List[Tuple[int, int]]):
+            seen = set()
+            for y, x in pos:
+                if (y, x) in seen:
+                    continue
+                res[y][x] += 1
+                seen.add((y, x))
+                for ny, nx in [[y + 1, x], [y - 1, x], [y, x + 1], [y, x - 1]]:
+                    if not (0 <= ny < m and
+                            0 <= nx < n) or heights[y][x] > heights[ny][nx]:
+                        continue
+                    pos.append((ny, nx))
+
+        fill([(0, x) for x in range(n)] + [(y, 0) for y in range(1, m)])
+        fill([(m - 1, x) for x in range(n)] + [(y, n - 1)
+                                               for y in range(m - 1)])
+        return [[y, x] for y, x in product(range(m), range(n))
+                if res[y][x] == 2]
+
+
+# @lc code=end
+if __name__ == "__main__":
+    run(MethodType(Solution.pacificAtlantic, Solution()), [
+        [[[[1, 2, 2, 3, 5], [3, 2, 3, 4, 4], [2, 4, 5, 3, 1], [6, 7, 1, 4, 5],
+           [5, 1, 1, 2, 4]]],
+         [[0, 4], [1, 3], [1, 4], [2, 2], [3, 0], [3, 1], [4, 0]]],
+        [[[[1]]], [[0, 0]]],
+    ],
+        comparator=any_order)
