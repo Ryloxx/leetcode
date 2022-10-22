a44ff193f0e979b0927046a47ad8d087f6847894
Rylox <in.telo3200@gmail.com>
2022-10-22 13:16:49 +0200

    76

---
+++ algorithms/76.minimum-window-substring.py
@@ -0,0 +1,107 @@
+#
+# @lc app=leetcode id=76 lang=python3
+#
+# [76] Minimum Window Substring
+#
+# https://leetcode.com/problems/minimum-window-substring/description/
+#
+# algorithms
+# Hard (39.08%)
+# Likes:    12988
+# Dislikes: 578
+# Total Accepted:    893.2K
+# Total Submissions: 2.2M
+# Testcase Example:  '"ADOBECODEBANC"\n"ABC"'
+#
+# Given two strings s and t of lengths m and n respectively, return the minimum
+# window substring of s such that every character in t (including duplicates)
+# is included in the window. If there is no such substring, return the empty
+# string "".
+#
+# The testcases will be generated such that the answer is unique.
+#
+# A substring is a contiguous sequence of characters within the string.
+#
+#
+# Example 1:
+#
+#
+# Input: s = "ADOBECODEBANC", t = "ABC"
+# Output: "BANC"
+# Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C'
+# from string t.
+#
+#
+# Example 2:
+#
+#
+# Input: s = "a", t = "a"
+# Output: "a"
+# Explanation: The entire string s is the minimum window.
+#
+#
+# Example 3:
+#
+#
+# Input: s = "a", t = "aa"
+# Output: ""
+# Explanation: Both 'a's from t must be included in the window.
+# Since the largest window of s only has one 'a', return empty string.
+#
+#
+#
+# Constraints:
+#
+#
+# m == s.length
+# n == t.length
+# 1 <= m, n <= 10^5
+# s and t consist of uppercase and lowercase English letters.
+#
+#
+#
+# Follow up: Could you find an algorithm that runs in O(m + n) time?
+#
+#
+from typing import Counter
+from algo_input import run
+from types import MethodType
+
+
+# @lc code=start
+class Solution:
+
+    def minWindow(self, s: str, t: str) -> str:
+        counter_t = Counter(t)
+        missing_char, res = len(counter_t), (-float("inf"), float('inf'))
+        left = right = 0
+        while left < len(s) and right <= len(s):
+            while right < len(s) and missing_char:
+                if s[right] in counter_t:
+                    counter_t[s[right]] -= 1
+                    missing_char -= not counter_t[s[right]]
+                right += 1
+            if not missing_char:
+                res = min(res, (left, right), key=lambda x: x[1] - x[0])
+            if s[left] in counter_t:
+                counter_t[s[left]] += 1
+                missing_char += counter_t[s[left]] == 1
+            left += 1
+        return "" if res[1] == float('inf') else s[res[0]:res[1]]
+
+
+# @lc code=end
+if __name__ == "__main__":
+    run(
+        MethodType(Solution.minWindow, Solution()),
+        [
+            (["ADOBECODEBANC", "ABC"], "BANC"),
+            (["a", "a"], "a"),
+            (["a", "aa"], ""),
+            (["", "aa"], ""),
+            (["ababbbbbbbbba", "aa"], "aba"),
+            (["", ""], ""),
+            (["abbbbbbbbbbba", "aa"], "abbbbbbbbbbba"),
+            (["bbbbbbbbbbaa", "aa"], "aa"),
+        ],
+    )
