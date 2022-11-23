2cf18746e3fa61a93650560541810c4245bec398
Rylox <in.telo3200@gmail.com>
2022-11-23 09:23:03 +0100

    36

---
+++ algorithms/36.valid-sudoku.py
@@ -0,0 +1,144 @@
+#
+# @lc app=leetcode id=36 lang=python3
+#
+# [36] Valid Sudoku
+#
+# https://leetcode.com/problems/valid-sudoku/description/
+#
+# algorithms
+# Medium (55.20%)
+# Likes:    7119
+# Dislikes: 828
+# Total Accepted:    940.1K
+# Total Submissions: 1.6M
+# Testcase Example:  '[["5","3",".",".","7",".",".",".","."]
+# ,["6",".",".","1","9","5",".",".","."]
+# ,[".","9","8",".",".",".",".","6","."]
+# ,["8",".",".",".","6",".",".",".","3"]
+# ,["4",".",".","8",".","3",".",".","1"]
+# ,["7",".",".",".","2",".",".",".","6"]
+# ,[".","6",".",".",".",".","2","8","."]
+# ,[".",".",".","4","1","9",".",".","5"]
+# ,[".",".",".",".","8",".",".","7","9"]]'
+#
+# Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be
+# validated according to the following rules:
+#
+#
+# Each row must contain the digits 1-9 without repetition.
+# Each column must contain the digits 1-9 without repetition.
+# Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9
+# without repetition.
+#
+#
+# Note:
+#
+#
+# A Sudoku board (partially filled) could be valid but is not necessarily
+# solvable.
+# Only the filled cells need to be validated according to the mentioned
+# rules.
+#
+#
+#
+# Example 1:
+#
+#
+# Input: board =
+# [["5","3",".",".","7",".",".",".","."]
+# ,["6",".",".","1","9","5",".",".","."]
+# ,[".","9","8",".",".",".",".","6","."]
+# ,["8",".",".",".","6",".",".",".","3"]
+# ,["4",".",".","8",".","3",".",".","1"]
+# ,["7",".",".",".","2",".",".",".","6"]
+# ,[".","6",".",".",".",".","2","8","."]
+# ,[".",".",".","4","1","9",".",".","5"]
+# ,[".",".",".",".","8",".",".","7","9"]]
+# Output: true
+#
+#
+# Example 2:
+#
+#
+# Input: board =
+# [["8","3",".",".","7",".",".",".","."]
+# ,["6",".",".","1","9","5",".",".","."]
+# ,[".","9","8",".",".",".",".","6","."]
+# ,["8",".",".",".","6",".",".",".","3"]
+# ,["4",".",".","8",".","3",".",".","1"]
+# ,["7",".",".",".","2",".",".",".","6"]
+# ,[".","6",".",".",".",".","2","8","."]
+# ,[".",".",".","4","1","9",".",".","5"]
+# ,[".",".",".",".","8",".",".","7","9"]]
+# Output: false
+# Explanation: Same as Example 1, except with the 5 in the top left corner
+# being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it
+# is invalid.
+#
+#
+#
+# Constraints:
+#
+#
+# board.length == 9
+# board[i].length == 9
+# board[i][j] is a digit 1-9 or '.'.
+#
+#
+#
+from types import MethodType
+from typing import List
+from algo_input import run
+
+
+# @lc code=start
+class Solution:
+
+    def isgood(self, board, i, j, n):
+        if n in board[i]:
+            return False
+        if not all(n not in board[x][j] for x in range(9)):
+            return False
+        a = (i // 3) * 3
+        b = (j // 3) * 3
+        return all(board[y][x] != n for y in range(a, a + 3)
+                   for x in range(b, b + 3))
+
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] != ".":
+                    temp = board[i][j]
+                    board[i][j] = "."
+                    if not self.isgood(board, i, j, temp):
+                        return False
+                    board[i][j] = temp
+        return True
+
+
+# @lc code=end
+
+if __name__ == "__main__":
+    run(
+        MethodType(Solution.isValidSudoku, Solution()),
+        [
+            ([[["5", "3", ".", ".", "7", ".", ".", ".", "."],
+               ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+               [".", "9", "8", ".", ".", ".", ".", "6", "."],
+               ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+               ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+               ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+               [".", "6", ".", ".", ".", ".", "2", "8", "."],
+               [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+               [".", ".", ".", ".", "8", ".", ".", "7", "9"]]], True),
+            ([[["8", "3", ".", ".", "7", ".", ".", ".", "."],
+               ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+               [".", "9", "8", ".", ".", ".", ".", "6", "."],
+               ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+               ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+               ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+               [".", "6", ".", ".", ".", ".", "2", "8", "."],
+               [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+               [".", ".", ".", ".", "8", ".", ".", "7", "9"]]], False),
+        ],
+    )
