99c64575141c62835afb0b5cbaf9fbee771bb4db
Ryloxx <in.telo3200@gmail.com>
2023-09-02 12:37:23 +0200

    format

--- algorithms/rust/src/bin/2707-extra-characters-in-a-string.rs
+++ algorithms/rust/src/bin/2707-extra-characters-in-a-string.rs
@@ -1,35 +1,41 @@
 /*
- * @lc app=leetcode id=2140 lang=rust
+ * @lc app=leetcode id=2707 lang=rust
  *
- * [2140] Solving Questions With Brainpower
+ * [2707] Extra Characters in a String
  *
- * https://leetcode.com/problems/solving-questions-with-brainpower/description/
+ * https://leetcode.com/problems/extra-characters-in-a-string/
  *
  * algorithms
- * Medium (46.10%)
- * Likes:    1557
- * Dislikes: 36
- * Total Accepted:    54.3K
- * Total Submissions: 102.2K
- * Testcase Example:  '[[3,2],[4,3],[4,4],[2,5]]'
+ * Medium (47.5%)
+ * Likes:    1096
+ * Dislikes: 51
+ * Total Accepted:    38.1K
+ * Total Submissions: 80.5K
+ * Testcase Example:  '"leetscode"\n["leet","code","leetcode"]'
  *
  * You are given a 0-indexed string s and a dictionary of words dictionary.
  * You have to break s into one or more non-overlapping substrings such that
  * each substring is present in dictionary. There may be some extra
  * characters in s which are not present in any of the substrings.
  * Return the minimum number of extra characters left over if you break up s
- * optimally. Example 1:
- *  Input: s = "leetscode", dictionary = ["leet","code","leetcode"]
- *  Output: 1
- *  Explanation: We can break s in two substrings: "leet" from index 0 to 3
+ * optimally. 
+ * 
+ * Example 1:
+ * Input: s = "leetscode", dictionary = ["leet","code","leetcode"]
+ * Output: 1
+ * Explanation: We can break s in two substrings: "leet" from index 0 to 3
  * and "code" from index 5 to 8. There is only 1 unused character (at index
- * 4), so we return 1.  Example 2:
- *  Input: s = "sayhelloworld", dictionary = ["hello","world"]
+ * 4), so we return 1.  
+ * 
+ * Example 2:
+ * Input: s = "sayhelloworld", dictionary = ["hello","world"]
  * Output: 3
  * Explanation: We can break s in two substrings: "hello" from index 3 to 7
  * and "world" from index 8 to 12. The characters at indices 0, 1, 2 are not
  * used in any substring and thus are considered as extra characters. Hence,
- * we return 3. Constraints:
+ * we return 3.
+ * 
+ * Constraints:
  * 1 <= s.length <= 50
  * 1 <= dictionary.length <= 50
  * 1 <= dictionary[i].length <= 50
