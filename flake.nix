f5c2f19c50c17554125c11ea117ea0421b261d89
rylox <in.telo3200@gmail.com>
2024-01-03 17:33:12 +0100

    nixify

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  outputs = { self, nixpkgs, flake-utils }:
+
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        pythonPackages = pkgs.python310Packages;
+        nodePackages = pkgs.nodePackages;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "ryloxx-mono";
+          venvDir = "./.venv";
+          buildInputs = with pythonPackages;[
+            python
+            black
+            flake8
+            venvShellHook
+          ] ++
+          (with nodePackages; [
+            prettier
+          ]) ++ (with pkgs;[
+            pipenv
+            rustup
+            nodejs_18
+            lint-staged
+            husky
+          ]);
+          postShellHook = ''
+            echo "pipenv install..."
+            pipenv sync --dev > .dev_shell_out
+          '';
+        };
+      });
+}
